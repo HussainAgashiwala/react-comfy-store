03ac98e6935084b0726ed8e800355ca8e6f60ef4
Hussain Agashiwala <hussainagashi83@gmail.com>
2022-08-26 00:25:13 +0530

    initial commit

---
+++ src/components/Filters.js
@@ -0,0 +1,249 @@
+import React from "react";
+import styled from "styled-components";
+import { useFilterContext } from "../context/filter_context";
+import { getUniqueValues, formatPrice } from "../utils/helpers";
+import { FaCheck } from "react-icons/fa";
+
+const Filters = () => {
+  const {
+    filters: {
+      text,
+      category,
+      company,
+      price,
+      min_price,
+      max_price,
+      shipping,
+      color,
+    },
+    updateFilters,
+    clearFilters,
+    all_products,
+  } = useFilterContext();
+
+  const categories = getUniqueValues(all_products, "category");
+  const companies = getUniqueValues(all_products, "company");
+  const colors = getUniqueValues(all_products, "colors");
+
+  return (
+    <Wrapper>
+      <div className="content">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <div className="form-control">
+            <input
+              name="text"
+              type="text"
+              className="search-input"
+              placeholder="search"
+              value={text}
+              onChange={updateFilters}
+            />
+          </div>
+          <div className="form-control">
+            <h5>Category</h5>
+            <div>
+              {categories.map((c, index) => {
+                return (
+                  <button
+                    type="button"
+                    key={index}
+                    className={`${
+                      category === c.toLowerCase() ? "active" : null
+                    }`}
+                    name="category"
+                    onClick={updateFilters}
+                  >
+                    {c}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+          <div className="form-control">
+            <h5>company</h5>
+            <select
+              name="company"
+              className="company"
+              value={company}
+              onChange={updateFilters}
+            >
+              {companies.map((c, index) => {
+                return (
+                  <option key={index} value={c}>
+                    {c}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="form-control">
+            <h5>Colors</h5>
+            <div className="colors">
+              {colors.map((c, index) => {
+                if (c === "all") {
+                  return (
+                    <button
+                      data-color="all"
+                      name="color"
+                      onClick={updateFilters}
+                      className={`${
+                        color === "all" ? "all-btn active" : "all-btn"
+                      }`}
+                    >
+                      all
+                    </button>
+                  );
+                }
+                return (
+                  <button
+                    key={index}
+                    type="button"
+                    name="color"
+                    onClick={updateFilters}
+                    data-color={c}
+                    className={`${
+                      color === c ? "color-btn active" : "color-btn"
+                    }`}
+                    style={{ background: c }}
+                  >
+                    {color === c ? <FaCheck /> : null}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+          <div className="form-control">
+            <h5>price</h5>
+            <p className="price">{formatPrice(price)}</p>
+            <input
+              type="range"
+              onChange={updateFilters}
+              name="price"
+              min={min_price}
+              max={max_price}
+              value={price}
+            />
+          </div>
+          <div className="form-control shipping">
+            <label htmlFor="shipping">free shipping</label>
+            <input
+              id="shipping"
+              type="checkbox"
+              name="shipping"
+              onChange={updateFilters}
+              checked={shipping}
+            />
+          </div>
+        </form>
+        <button type='button' onClick={clearFilters} className='clear-btn'>
+          clear filters
+        </button>
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  .form-control {
+    margin-bottom: 1.25rem;
+    h5 {
+      margin-bottom: 0.5rem;
+    }
+  }
+  .search-input {
+    padding: 0.5rem;
+    background: var(--clr-grey-10);
+    border-radius: var(--radius);
+    border-color: transparent;
+    letter-spacing: var(--spacing);
+  }
+  .search-input::placeholder {
+    text-transform: capitalize;
+  }
+
+  button {
+    display: block;
+    margin: 0.25em 0;
+    padding: 0.25rem 0;
+    text-transform: capitalize;
+    background: transparent;
+    border: none;
+    border-bottom: 1px solid transparent;
+    letter-spacing: var(--spacing);
+    color: var(--clr-grey-5);
+    cursor: pointer;
+  }
+  .active {
+    border-color: var(--clr-grey-5);
+  }
+  .company {
+    background: var(--clr-grey-10);
+    border-radius: var(--radius);
+    border-color: transparent;
+    padding: 0.25rem;
+  }
+  .colors {
+    display: flex;
+    align-items: center;
+  }
+  .color-btn {
+    display: inline-block;
+    width: 1rem;
+    height: 1rem;
+    border-radius: 50%;
+    background: #222;
+    margin-right: 0.5rem;
+    border: none;
+    cursor: pointer;
+    opacity: 0.5;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    svg {
+      font-size: 0.5rem;
+      color: var(--clr-white);
+    }
+  }
+  .all-btn {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 0.5rem;
+    opacity: 0.5;
+  }
+  .active {
+    opacity: 1;
+  }
+  .all-btn .active {
+    text-decoration: underline;
+  }
+  .price {
+    margin-bottom: 0.25rem;
+  }
+  .shipping {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    align-items: center;
+    text-transform: capitalize;
+    column-gap: 0.5rem;
+    font-size: 1rem;
+  }
+  .clear-btn {
+    background: var(--clr-red-dark);
+    color: var(--clr-white);
+    padding: 0.25rem 0.5rem;
+    border-radius: var(--radius);
+  }
+  @media (min-width: 768px) {
+    .content {
+      position: sticky;
+      top: 1rem;
+    }
+  }
+`;
+
+export default Filters;
